beb0c74c7df573dd3ff0ff650a7b0a55b4381ac1
shuhey <shuhey.dev@gmail.com>
2018-10-13 11:19:43 +0900

    プロジェクト ファイルを追加します。

---
+++ AccessToFilesInProject/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccessToFilesInProject
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var textFileContent = Properties.Resources.ThisIsTextFile;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "AccessToFilesInProject.Resources.マニュアル.ThisIsManual1.txt";
+
+            string manualFileContent;
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream != null)
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        manualFileContent = sr.ReadToEnd();
+                    }
+                }
+            }
+
+        }
+    }
+}
